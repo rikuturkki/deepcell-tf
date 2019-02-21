1f0a120f0d2781a7a05e3eba871b2efe9c979bde
circuit1 <ccui@caltech.edu>
2019-02-21 10:15:43 -0800

    fixing LSTM

--- scripts/recurr_gru/train_LSTM.py
+++ scripts/recurr_gru/train_LSTM.py
@@ -99,17 +99,14 @@ def feature_net_LSTM(input_shape,
 
     model = Sequential()
 
-    # model.add(InputLayer(input_shape=input_shape))
+    model.add(InputLayer(input_shape=input_shape))
     # model.add(ImageNormalization3D(norm_method=norm_method, filter_size=receptive_field))
-    model.add(ConvGRU2D(n_conv_filters, 
-            input_shape=input_shape,
-            kernel_size=(filter_size, filter_size), 
-            padding='same', 
-            kernel_initializer=init,
-            kernel_regularizer=l2(reg), 
-            activation='relu', 
-            return_sequences=True))
+    model.add(ConvLSTM2D(filters=n_conv_filters, kernel_size=(3, 3),
+                       padding='same',
+                       kernel_initializer=init,
+                       kernel_regularizer=l2(reg), return_sequences=True))
 
+    '''
     rf_counter = receptive_field
     block_counter = 0
     d = 1
@@ -128,6 +125,7 @@ def feature_net_LSTM(input_shape,
         rf_counter -= filter_size - 1
 
         rf_counter = rf_counter // 2
+    '''
 
     model.add(ConvLSTM2D(filters=n_conv_filters, kernel_size=(3, 3),
                        padding='same',
