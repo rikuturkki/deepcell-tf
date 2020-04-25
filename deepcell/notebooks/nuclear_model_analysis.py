2f18230f8db5d3e4862c9ab3d0894bd1a680a420
circuit1 <ccui@caltech.edu>
2020-04-24 18:51:07 -0700

    new branch

---
+++ deepcell/notebooks/nuclear_model_analysis.py
@@ -0,0 +1,46 @@
+import os
+import papermill as pm
+
+filename = 'general_nuclear.npz'
+date = '04242020'
+
+dataset_split_seeds = [0] # [0, 1, 2]
+dataset_fractions = [0.1] #[0.01, 0.1, 0.25, 0.5, 1]
+backbones = ['resnet50', 'mobilenetv2']
+model_types = ['retinamask', 'pixelwise', 'watershed']
+
+for seed in dataset_split_seeds:
+    for dataset_fraction in dataset_fractions:
+        for backbone in backbones:
+            for model_type in model_types:
+                filename = 'nuclear_{}_{}_{}_{}'.format(seed, dataset_fraction, backbone, model_type)
+                
+                print('Processing {}'.format(filename))
+                
+                # Define notebook paths
+                input_notebook_name = 'Papermill - Nuclear Accuracy - Proper Split.ipynb'
+                output_notebook_name = filename + '_training_notebook.ipynb'
+                
+                input_direc = os.path.join('/notebooks', 'papermill')
+                output_direc = os.path.join('/notebooks', date)
+                
+                os.makedirs(input_direc, exist_ok=True)
+                os.makedirs(output_direc, exist_ok=True)
+                
+                input_path = os.path.join(input_direc, input_notebook_name)
+                output_path = os.path.join(output_direc, output_notebook_name)
+                
+                # Define parameters
+                parameters = {'n_epochs':16, 
+                              'batch_size':4 if model_type == 'retinamask' else 16, 
+                              'date': date, 
+                              'filename': filename,
+                              'train_permutation_seed': seed,
+                              'dataset_fraction': dataset_fraction,
+                              'backbone':backbone,
+                              'model_type': model_type}
+                
+                # Run notebook
+                pm.execute_notebook(input_path,
+                                    output_path,
+                                    parameters=parameters)
