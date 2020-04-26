6659e170769f58bb5e845fe607cf6a0de522e4a0
circuit1 <ccui@caltech.edu>
2020-04-26 00:38:26 +0000

    ran a few more

--- deepcell/notebooks/nuclear_model_analysis.py
+++ deepcell/notebooks/nuclear_model_analysis.py
@@ -2,12 +2,12 @@ import os
 import papermill as pm
 
 filename = 'general_nuclear.npz'
-date = '04242020'
+date = '04252020'
 
-dataset_split_seeds = [0] # [0, 1, 2]
-dataset_fractions = [0.5] #[0.01, 0.1, 0.25, 0.5, 1]
-backbones = ['resnet50', 'mobilenetv2']
-model_types = ['retinamask', 'pixelwise', 'watershed']
+dataset_split_seeds = [1] # [0, 1, 2]
+dataset_fractions = [0.25] #[0.01, 0.1, 0.25, 0.5, 1]
+backbones = ['resnet50', 'mobilenetv2'] # 'resnet50'
+model_types = ['pixelwise', 'retinamask',  'watershed']
 
 for seed in dataset_split_seeds:
     for dataset_fraction in dataset_fractions:
